68e51506bcb53482ab1838129dc4de2dc4c30bdf
Oskar Jönsson <37797439+oskarjonszon@users.noreply.github.com>
2021-02-21 12:16:58 +0100

    Code

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        int[][] table = new int[][] {
+
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+        };
+
+        Backtracing backtracing = new Backtracing();
+        backtracing.solve(table);
+    }
+}
